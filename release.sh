83601aa6c337553fe826504ca05683dbd0ec92e8
Gray Watson <gray.github@mailgw.com>
2026-04-21 17:34:26 -0400

    cp

--- release.sh
+++ release.sh
@@ -3,8 +3,20 @@
 # Release script
 #
 
-LIBRARY="simplecsv"
+LIBRARY=`basename $(pwd)`
 LOCAL_DIR="$HOME/svn/local/$LIBRARY"
+
+#############################################################
+
+release=$(grep version pom.xml | grep SNAPSHOT | head -1 | cut -f2 -d\> | cut -f1 -d\-)
+
+/bin/echo ""
+/bin/echo "------------------------------------------------------- "
+/bin/echo -n "Enter ${LIBRARY} release number [$release]: "
+read rel
+if [ "$rel" != "" ]; then
+    release=$rel
+fi
 
 #############################################################
 # check initial stuff
@@ -13,14 +25,14 @@ bad=0
 
 git status | head -1 | fgrep main > /dev/null 2>&1
 if [ $? -ne 0 ]; then
-    /bin/echo "Should be on main branch."
+    /bin/echo "ERROR: Should be on main branch."
     git status | head -1
     bad=1
 fi
 
 head -1 src/main/javadoc/doc-files/changelog.txt | fgrep '?' > /dev/null 2>&1
 if [ $? -ne 1 ]; then
-    /bin/echo "No question-marks (?) can be in the ChangeLog top line."
+    /bin/echo "ERROR: No question-marks (?) can be in the ChangeLog top line."
     head -1 src/main/javadoc/doc-files/changelog.txt
     bad=1
 fi
@@ -28,30 +40,23 @@ fi
 cd $LOCAL_DIR
 git status | grep 'nothing to commit'
 if [ $? -ne 0 ]; then
-    /bin/echo "Files not checked-in"
+    /bin/echo "ERROR: Files not checked-in"
     git status
     bad=1
 fi
 
-#############################################################
-# check maven settings
-
-grep sonatype-nexus-snapshots $HOME/.m2/settings.xml > /dev/null 2>&1
+echo "running javadoc tests"
+mvn -B -q javadoc:javadoc
 if [ $? -ne 0 ]; then
-    /bin/echo "Can't find sonatype info in the maven settings.xml file"
+    /bin/echo "ERROR: javadoc errors exist"
     bad=1
 fi
 
-#############################################################
-
-release=$(grep version pom.xml | grep SNAPSHOT | head -1 | cut -f2 -d\> | cut -f1 -d\-)
-
-/bin/echo ""
-/bin/echo "------------------------------------------------------- "
-/bin/echo -n "Enter release number [$release]: "
-read rel
-if [ "$rel" != "" ]; then
-    release=$rel
+cd $LOCAL_DIR
+mvn javadoc:javadoc | grep WARNING
+if [ $? -eq 0 ]; then
+    /bin/echo "ERROR: javadoc warnings exist"
+    bad=1
 fi
 
 #############################################################
@@ -60,21 +65,21 @@ fi
 cd $LOCAL_DIR
 ver=$(head -1 src/main/javadoc/doc-files/changelog.txt | cut -f1 -d:)
 if [ "$release" != "$ver" ]; then
-    /bin/echo "Change log top line version seems wrong:"
+    /bin/echo "ERROR: Change log top line version seems wrong:"
     head -1 src/main/javadoc/doc-files/changelog.txt
     bad=1
 fi
 
 grep -q $release README.md
 if [ $? != 0 ]; then
-    /bin/echo "Could not find $release in README.md"
+    /bin/echo "ERROR: Could not find $release in README.md"
     bad=1
 fi
 
 if [ -r "src/main/doc/$LIBRARY.texi" ]; then
     ver=$(grep "^@set ${LIBRARY}_version" src/main/doc/$LIBRARY.texi | cut -f3 -d' ')
     if [ "$release" != "$ver" ]; then
-	/bin/echo "$LIBRARY.texi version seems wrong:"
+	/bin/echo "ERROR: $LIBRARY.texi version seems wrong:"
 	grep "^@set ${LIBRARY}_version" src/main/doc/$LIBRARY.texi
 	bad=1
     fi
@@ -83,7 +88,7 @@ fi
 if [ -r "src/main/javadoc/doc-files/$LIBRARY.html" ]; then
     grep "Version $release" src/main/javadoc/doc-files/$LIBRARY.html > /dev/null
     if [ $? -ne 0 ]; then
-	/bin/echo "javadoc doc-files $LIBRARY.html version seems wrong:"
+	/bin/echo "ERROR: javadoc doc-files $LIBRARY.html version seems wrong:"
 	grep "Version " src/main/javadoc/doc-files/$LIBRARY.html
 	bad=1
     fi
@@ -121,10 +126,9 @@ git push --delete origin $tag 2> /dev/null
 read cont
 if [ "$cont" = "" -o "$cont" = "y" ]; then
     cd $LOCAL_DIR
-    mvn -P st release:clean || exit 1
-    mvn -P st release:prepare || ( /bin/echo "Maybe use mvn release:rollback to rollback"; exit 1 )
-    mvn -P st release:perform || ( /bin/echo "Maybe use mvn release:rollback to rollback"; exit 1 )
-
+    mvn release:clean || exit 1
+    mvn release:prepare || { /bin/echo "Maybe use mvn release:rollback to rollback"; exit 1; }
+    mvn release:perform || { /bin/echo "Maybe use mvn release:rollback to rollback"; exit 1; }
     /bin/echo ""
     /bin/echo ""
 fi
